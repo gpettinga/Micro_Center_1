1a81cab4ec2e6056530020ce492ebed495f400b1
Graham <gpettinga@gmail.com>
2015-05-24 15:29:08 -0500

    Camera switching implemented, as well as created camera controls.  Lights are off in the test scene

---
+++ Assets/Scripts/SimpleCameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCameraMovement : MonoBehaviour {
+
+	public float moveSpeed	;
+	private float moveX;
+	private float moveY;
+	private float moveZ;
+	private Vector3 camMover;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		moveX = Input.GetAxis ("Horizontal");
+		moveZ= Input.GetAxis ("Vertical");
+		moveY = Input.GetAxis("Mouse ScrollWheel");
+
+		camMover = new Vector3(moveX,moveY*10,moveZ) * Time.deltaTime * moveSpeed;
+
+		transform.Translate(camMover);
+
+
+
+	}
+}
---
+++ Assets/Scripts/SwapCameras.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SwapCameras : MonoBehaviour {
+
+	public GameObject fpsCamera;
+	public GameObject overHeadCamera;
+
+	public bool fpsActive = true;
+
+	public GameObject[] roofs;
+
+	// Use this for initialization
+	void Start () 
+	{
+		roofs = GameObject.FindGameObjectsWithTag("OverHead");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if(Input.GetMouseButtonDown(2))
+		{
+			Swap ();
+		}
+
+		if (fpsActive)
+		{
+			fpsCamera.SetActive(true);
+			overHeadCamera.SetActive(false);
+			foreach (GameObject obj in roofs)
+			{
+				obj.SetActive(true);
+			}
+		}
+		else
+		{
+			fpsCamera.SetActive(false);
+			overHeadCamera.SetActive(true);
+			foreach (GameObject obj in roofs)
+			{
+				obj.SetActive(false);
+			}
+		}
+
+
+
+	}
+
+	void Swap()
+	{
+		fpsActive = !fpsActive;
+	}
+}
